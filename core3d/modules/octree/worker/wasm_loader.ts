4ec3dc9ce59025afae2ab101d5f910b6f647b1eb
Arturo Castro <arturo@rin.rs>
2023-11-07 15:00:39 +0100

    wasm-bindgen target web

--- core3d/modules/octree/worker/wasm_loader.ts
+++ core3d/modules/octree/worker/wasm_loader.ts
@@ -1,16 +1,27 @@
 import type * as WasmNamespace from "@novorender/wasm-parser";
-// @ts-ignore
-import * as wasmWrapper from "@novorender/wasm-parser/wasm_parser_bg";
+
+// For wasm bindgen target web
+import * as wasmWrapper from "@novorender/wasm-parser/wasm_parser";
+
+// For wasm bindgen target bundler
+// // @ts-ignore
+// import * as wasmWrapper from "@novorender/wasm-parser/wasm_parser_bg";
 
 export type WasmInstance = typeof WasmNamespace;
 
 /** @internal */
 export async function esbuildWasmInstance(wasmData: ArrayBuffer): Promise<WasmInstance> {
-    let imports = {
-        ["./wasm_parser_bg.js"]: wasmWrapper,
-    };
-    const { instance } = await WebAssembly.instantiate(wasmData, imports);
-    wasmWrapper.__wbg_set_wasm(instance.exports);
+    // For wasm bindgen target bundler
+    // let imports = {
+    //     ["./wasm_parser_bg.js"]: wasmWrapper,
+    // };
+    // const { instance } = await WebAssembly.instantiate(wasmData, imports);
+    // wasmWrapper.__wbg_set_wasm(instance.exports);
+
+    // For wasm bindgen target web
+    await wasmWrapper.default(wasmData);
+
+    // Common
     wasmWrapper.init_console();
     return wasmWrapper;
 }
